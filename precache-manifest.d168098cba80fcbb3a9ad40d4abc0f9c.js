e863fba7684a9fc9617fc1a197023297bed15070
Chris Garcia <pixelbandito@gmail.com>
2018-12-01 13:34:39 -0600

    Updates

---
+++ precache-manifest.d168098cba80fcbb3a9ad40d4abc0f9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/musico/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "498a023abe98c768bb5c",
+    "url": "/musico/static/js/runtime~main.498a023a.js"
+  },
+  {
+    "revision": "2031ffbe2a318222f664",
+    "url": "/musico/static/js/main.2031ffbe.chunk.js"
+  },
+  {
+    "revision": "d855c10c80aa95827f2a",
+    "url": "/musico/static/js/1.d855c10c.chunk.js"
+  },
+  {
+    "revision": "2031ffbe2a318222f664",
+    "url": "/musico/static/css/main.0d790245.chunk.css"
+  },
+  {
+    "revision": "d794730fca9dd32f9ff83dfcd792040b",
+    "url": "/musico/index.html"
+  }
+];
