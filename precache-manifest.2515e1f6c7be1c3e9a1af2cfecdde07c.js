a7366464b531d67d3da54e84889b22ba1e24cc7d
Chris Garcia <pixelbandito@gmail.com>
2018-12-05 09:22:47 -0600

    Updates

--- precache-manifest.2515e1f6c7be1c3e9a1af2cfecdde07c.js
+++ precache-manifest.2515e1f6c7be1c3e9a1af2cfecdde07c.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/musico/static/js/runtime~main.498a023a.js"
   },
   {
-    "revision": "f0c369c09634a27144c8",
-    "url": "/musico/static/js/main.f0c369c0.chunk.js"
+    "revision": "1c81dc9bca9c91ec6c8b",
+    "url": "/musico/static/js/main.1c81dc9b.chunk.js"
   },
   {
     "revision": "344d0b00d98545086112",
     "url": "/musico/static/js/1.344d0b00.chunk.js"
   },
   {
-    "revision": "f0c369c09634a27144c8",
+    "revision": "1c81dc9bca9c91ec6c8b",
     "url": "/musico/static/css/main.8c59f0ff.chunk.css"
   },
   {
-    "revision": "a3213d448cbf91186a14b867b2271178",
+    "revision": "4033b7c9a93b009a959fa538fb5dcf72",
     "url": "/musico/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/musico/precache-manifest.9c331e0428f7a808446a5c3e35f25412.js"
+  "/musico/precache-manifest.2515e1f6c7be1c3e9a1af2cfecdde07c.js"
 );
 
 workbox.clientsClaim();
